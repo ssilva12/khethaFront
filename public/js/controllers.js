32b7e29d919402fd677c8989cd57cbe413be5e71
Sergio Daniel Silva Saldarriaga <ssilva@intergrupo.com>
2017-11-28 13:42:53 -0500

    rutas test

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -68,8 +68,8 @@ controller('SynonymsCtrl', function ($scope,$location,Dictionary,termFactory,Upl
       }
     })
   };
-  //var url = 'http://polar-garden-35450.herokuapp.com'
-  var url = 'http://localhost:3000'
+  var url = 'http://polar-garden-35450.herokuapp.com'
+  //var url = 'http://localhost:3000'
   $scope.uploadFiles = function(file,type) {
     if(type=="primary"){
       var route = "/upload"
