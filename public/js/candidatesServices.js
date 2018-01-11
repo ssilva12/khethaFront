450cdb759a66c7e04719fad421fc46b1e0df291f
jegs26 <jegs26@hotmail.com>
2018-01-11 14:56:27 -0500

    Modificaciones al detalle del candidato

--- public/js/candidatesServices.js
+++ public/js/candidatesServices.js
@@ -1,52 +1,91 @@
 angular.module('myApp.candidatesServices', [])
-    .factory('candidatesServices', function ($http) {
+    .factory('candidatesServices', ['$http', 'URL', function ($http, URL) {
 
         var candidatesServices = {};
-        var url = "http://localhost:9000/"
-        //var url = "http://guarded-atoll-31281.herokuapp.com/"
+        var Result = {};
+        Result.error = false;
+        Result.status = null;
+        Result.message = "";
+        Result.data = null;
+        
         candidatesServices.uploadFile = function (file) {
             var fd = new FormData();
             fd.append('curriculum', file);
-            var uploadUrl = url+'uploadCv';
-            //debugger;
+            var uploadUrl = URL.URL_REST_SERVICE + 'uploadCv';
             $http.post(uploadUrl, fd, {
-                transformRequest: angular.identity,
-                headers: {'Content-Type': undefined}
-            })
-            .success(function(){
-            })
-            .error(function(){
-            });
+                    transformRequest: angular.identity,
+                    headers: {
+                        'Content-Type': undefined
+                    }
+                })
+                .success(function () {})
+                .error(function () {});
         };
 
-        candidatesServices.getAll = function () {
-            var Datos = $http.get('http://guarded-atoll-31281.herokuapp.com/getCandidates', {
-                headers: {
-                    "Content-Type": "application/json",
-                    "Access-Control-Allow-Origin": "*",
-                    "Access-Control-Allow-Methods": "GET",
-                    "Access-Control-Allow-Headers": "X-Requested-With"
-                }
-            }).then(function (res) {
-                return res.data;
-            }, function (error) {
-                return error;
-            });
-            return Datos;
-        };
-        candidatesServices.getById = function (id,callback) {
+        candidatesServices.getAll = function (callback) {
             $http({
                 method: 'GET',
-                params: {id: id},
-                url: url+'candidate'
+                url: URL.URL_REST_SERVICE + 'getCandidates'
             }).
             success(function (data, status, headers, config) {
-                callback(null,data)
+                Result.error = false;
+                Result.status = status;
+                Result.message = "OK";
+                Result.data = data;
+                callback(Result);
             }).
             error(function (data, status, headers, config) {
-                callback("Error")
+                Result.error = true;
+                Result.status = status;
+                switch (status) {
+                    case 404:
+                        Result.message = "Servicio no encontrado(" + URL.URL_REST_SERVICE + 'getCandidates).';
+                        break;
+                    case 500:
+                        Result.message = "Error en el servicio.";
+                        break;
+                    default:
+                        Result.message = "Error.";
+                        break;
+                }
+                Result.data = data;
+                callback(Result);
             });
-            }
+        };
+
+        candidatesServices.getById = function (id, callback) {
+            $http({
+                method: 'GET',
+                params: {
+                    id: id
+                },
+                url: URL.URL_REST_SERVICE + 'candidate'
+            }).
+            success(function (data, status, headers, config) {
+                Result.error = false;
+                Result.status = status;
+                Result.message = "OK";
+                Result.data = data;
+                callback(Result);
+            }).
+            error(function (data, status, headers, config) {
+                Result.error = true;
+                Result.status = status;
+                switch (status) {
+                    case 404:
+                        Result.message = "Servicio no encontrado(" + URL.URL_REST_SERVICE + 'candidate).';
+                        break;
+                    case 500:
+                        Result.message = "Error en el servicio.";
+                        break;
+                    default:
+                        Result.message = "Error.";
+                        break;
+                }
+                Result.data = data;
+                callback(Result);
+            });
+        }
 
         return candidatesServices;
-    });
+    }]);
--- public/js/filters.js
+++ public/js/filters.js
@@ -3,8 +3,17 @@
 /* Filters */
 
 angular.module('myApp.filters', []).
-  filter('interpolate', function (version) {
-    return function (text) {
-      return String(text).replace(/\%VERSION\%/mg, version);
-    };
-  });
+filter('interpolate', function (version) {
+  return function (text) {
+    return String(text).replace(/\%VERSION\%/mg, version);
+  };
+}).
+filter('startFrom', function () {
+  return function (input, start) {
+    if (input) {
+      start = +start;
+      return input.slice(start);
+    }
+    return [];
+  };
+});
