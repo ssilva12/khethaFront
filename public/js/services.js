18fab3bb390ec882723b0a30f5af97733d4867ca
Emilio Botero <e.botero10@uniandes.edu.co>
2018-01-17 08:41:53 -0500

    alert when synonym exists

--- public/js/services.js
+++ public/js/services.js
@@ -8,8 +8,8 @@
 angular.module('myApp.services', []).
   value('version', '0.1')
   .service('frequencyMatrixService', function($http) {
-    // var url = "http://localhost:9000/"
-    var url = "http://guarded-atoll-31281.herokuapp.com/"
+    var url = "http://localhost:9000/"
+    //var url = "http://guarded-atoll-31281.herokuapp.com/"
 
     this.get = (parameters, callback) => {
       $http({
@@ -85,8 +85,8 @@ angular.module('myApp.services', []).
 
   })
   .service('Dictionary', function($http) {
-    // var url = "http://localhost:9000/"
-    var url = "http://guarded-atoll-31281.herokuapp.com/"
+    var url = "http://localhost:9000/"
+    //var url = "http://guarded-atoll-31281.herokuapp.com/"
     this.getSynonyms = function (name,dictionaryName,acronym,callback) {
       if(dictionaryName == undefined){
         dictionaryName = "null"
@@ -126,9 +126,11 @@ angular.module('myApp.services', []).
         url: url+'createPrimary'
       }).
       success(function (data, status, headers, config) {
+          console.log(data)
         callback(null,data)
       }).
       error(function (data, status, headers, config) {
+          console.log(data)
         callback("Error")
       });
     }
@@ -142,6 +144,10 @@ angular.module('myApp.services', []).
         url: url+'createSynonyms'
       }).
       success(function (data, status, headers, config) {
+          console.log(data)
+          if (data.synonym.error) {
+              alert(data.synonym.error)
+          }
         callback(null,data)
       }).
       error(function (data, status, headers, config) {
