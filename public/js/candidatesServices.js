a0262232060a3961bcb35e20bf0578cfeee9c629
jegs26 <jegs26@hotmail.com>
2018-02-26 16:42:12 -0500

    Modificaciones ficha candidato

--- public/js/candidatesServices.js
+++ public/js/candidatesServices.js
@@ -293,5 +293,42 @@ angular.module('myApp.candidatesServices', [])
             });
         };
 
+        candidatesServices.getVacancyInfo = function (vacancyId, candidateId, callback) {
+            $http({
+                method: 'GET',
+                params: {
+                    jobVacancy: vacancyId,
+                    candidate: candidateId
+                },
+                url: URL.URL_REST_SERVICE + 'api/vacancy/candidate-match',
+                headers: {
+                    'Content-Type': "application/json"
+                }
+            }).
+                then(function onSuccess(response) {
+                    Result.error = false;
+                    Result.status = response.status;
+                    Result.message = "OK";
+                    Result.data = response.data;
+                    callback(Result);
+                }, function onError(response) {
+                    Result.error = true;
+                    Result.status = response.status;
+                    switch (status) {
+                        case 404:
+                            Result.message = "Servicio no encontrado(" + URL.URL_REST_SERVICE + 'api/vacancy/candidate-match).';
+                            break;
+                        case 500:
+                            Result.message = "Error en el servicio.";
+                            break;
+                        default:
+                            Result.message = "Error.";
+                            break;
+                    }
+                    Result.data = response.data;
+                    callback(Result);
+                });
+        };
+
         return candidatesServices;
     }]);
