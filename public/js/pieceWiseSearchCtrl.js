3df6d305d6652fe0693adbe2552a12ff90626691
Sergio Daniel Silva Saldarriaga <ssilva@intergrupo.com>
2018-03-16 13:53:49 -0500

    piece wise controller

---
+++ public/js/pieceWiseSearchCtrl.js
@@ -0,0 +1,13 @@
+// angular.module('myApp.pieceWiseSearchCtrl', ['ui.bootstrap', 'ngCookies']).
+// controller('pieceWiseSearchCtrl', ['$scope','termFactory', '$state', 'Mensaje', '$rootScope', 'Dictionary', '$parse', '$timeout', function ($scope, termFactory,candidatesServices, $state, Mensaje, $rootScope, Dictionary, $parse, $timeout, keepData) {
+//     $scope.current = termFactory.getCurrent();
+//     console.log($scope.current);
+//     pieceWiseSearch($scope.current.name,$scope.current.dictionary)
+//     function pieceWiseSearch(name,dictionaryName) {
+//         Dictionary.pieceWiseSearch(name, dictionaryName, function (error, data) {
+//             if (!error) {
+//                 console.log(data)
+//             }
+//         });
+//     }
+// }]);
