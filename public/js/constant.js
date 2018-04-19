cc624a70906cdda0bb4a09d5a713b180eb7ad45c
Sergio Daniel Silva Saldarriaga <ssilva@intergrupo.com>
2018-04-19 11:45:42 -0500

    apuntar a khetha-back heroku

--- public/js/constant.js
+++ public/js/constant.js
@@ -1,5 +1,6 @@
 angular.module('myApp.constant', [])
     .constant('URL', {
-        'URL_REST_SERVICE': 'http://guarded-atoll-31281.herokuapp.com/'
+        //'URL_REST_SERVICE': 'http://guarded-atoll-31281.herokuapp.com/'
+        'URL_REST_SERVICE': 'http://khetha-back.herokuapp.com/'
         //'URL_REST_SERVICE': 'http://localhost:9000/'
     });
